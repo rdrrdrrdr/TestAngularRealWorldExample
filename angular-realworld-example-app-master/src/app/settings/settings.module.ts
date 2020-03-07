81e037516f911da4d126f920012a9e681d8b52da
RichRodriguez <rdr@sprintmail.com>
2020-03-07 10:33:44 -0500

    sending to New Branch rdr

--- angular-realworld-example-app-master/src/app/settings/settings.module.ts
+++ angular-realworld-example-app-master/src/app/settings/settings.module.ts
@@ -2,10 +2,10 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SettingsComponent } from './settings.component';
+import { AboutmeComponent} from '../aboutme/aboutme.component';
 import { AuthGuard } from '../core';
 import { SharedModule } from '../shared';
 import { SettingsRoutingModule } from './settings-routing.module';
-
 
 
 @NgModule({
